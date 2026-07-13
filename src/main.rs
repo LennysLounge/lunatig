c171947f38c95d6031b86f848926c67a7ab23e4a
Leonard Schüngel <lschuengel@acardo.com>
2026-07-13 14:54:53 +0200

    Create a simple stage/unstage and commit ui

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::ops::Mul;
+use std::path::Path;
 
 use eframe::{
     App,
-    egui::{self, ThemePreference},
+    egui::{self, ThemePreference, Widget},
 };
-use git2::Repository;
+use git2::{ObjectType, Repository, StatusOptions};
 
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
         ..Default::default()
     };
 
@@ -23,62 +23,128 @@ fn main() -> eframe::Result<()> {
 }
 
 fn new_app() -> MyApp {
+    let repo = Repository::open("./").expect("Cannot open repository");
 
-    let repo = Repository::open("../egui_ltreeview/").expect("Cannot open repository");
-    let mut revwalk = repo.revwalk().unwrap();
-    revwalk.push_head().unwrap();
-
-    let mut commits = Vec::new();
-    for oid in revwalk{
-        let oid = oid.unwrap();
-        let commit = repo.find_commit(oid).unwrap();
-        commits.push(commit.message().unwrap().to_owned());
-    }
-
+    println!("head is branch: {}", repo.head().unwrap().is_branch());
+    println!("head detached: {}", repo.head_detached().unwrap());
 
     MyApp {
-        name: "Arthur".to_owned(),
-        age: 42,
         repo,
-        commits
+        commit_message: String::new(),
+        commit_ammend: false,
     }
 }
 
 struct MyApp {
-    name: String,
-    age: i32,
     repo: Repository,
-    commits: Vec<String>,
+    commit_message: String,
+    commit_ammend: bool,
 }
 impl App for MyApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        egui::MenuBar::default().ui(ui, |ui| {
-            ui.label("hi");
-            _ = ui.button("file");
-            ui.menu_button("file", |ui| {
-                ui.label("hi");
-            });
-        });
+        
         egui::CentralPanel::default().show(ui, |ui| {
-            ui.heading("Commits:");
-            for msg in self.commits.iter(){
-                ui.label(msg);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                
+                ui.heading("Unstaged:");
+                let statuses = self
+                    .repo
+                    .statuses(Some(
+                        &mut StatusOptions::new()
+                            .show(git2::StatusShow::Workdir)
+                            .include_ignored(false)
+                            .include_untracked(true)
+                            .recurse_untracked_dirs(true),
+                    ))
+                    .unwrap();
+                for status in statuses.iter() {
+                    ui.horizontal(|ui| {
+                        if ui.button("stage").clicked() {
+                            let mut index = self.repo.index().unwrap();
+                            index.add_path(Path::new(status.path().unwrap())).unwrap();
+                            index.write().unwrap();
+                        }
+                        if status.status().is_wt_new() {
+                            ui.monospace("NEW");
+                        }
+                        if status.status().is_wt_modified() {
+                            ui.monospace("MOD");
+                        }
+                        if status.status().is_wt_deleted() {
+                            ui.monospace("DEL");
+                        }
+                        if status.status().is_wt_typechange() {
+                            ui.monospace("TYP");
+                        }
+                        ui.label(status.path().unwrap());
+                    });
+                }
                 ui.separator();
-                // if !ui.clip_rect().intersects(ui.cursor()){
-                //     break;
-                // }
-            }
-            // ui.heading("My egui Application");
-            // ui.horizontal(|ui| {
-            //     let name_label = ui.label("Your name: ");
-            //     ui.text_edit_singleline(&mut self.name)
-            //         .labelled_by(name_label.id);
-            // });
-            // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            // if ui.button("Increment").clicked() {
-            //     self.age += 1;
-            // }
-            // ui.label(format!("Hello '{}', age {}", self.name, self.age));
+                ui.heading("Staged:");
+                let statuses = self
+                    .repo
+                    .statuses(Some(
+                        &mut StatusOptions::new()
+                            .show(git2::StatusShow::Index)
+                            .include_ignored(false)
+                            .include_untracked(true),
+                    ))
+                    .unwrap();
+                for status in statuses.iter() {
+                    ui.horizontal(|ui| {
+                        if ui.button("unstage").clicked() {
+                            let head = self.repo.head().unwrap().peel(ObjectType::Commit).unwrap();
+                            self.repo
+                                .reset_default(Some(&head), [Path::new(status.path().unwrap())])
+                                .unwrap();
+                        }
+                        if status.status().is_index_new() {
+                            ui.monospace("NEW");
+                        }
+                        if status.status().is_index_modified() {
+                            ui.monospace("MOD");
+                        }
+                        if status.status().is_index_deleted() {
+                            ui.monospace("DEL");
+                        }
+                        if status.status().is_index_typechange() {
+                            ui.monospace("TYP");
+                        }
+                        ui.label(status.path().unwrap());
+                    });
+                }
+                ui.separator();
+                egui::text_edit::TextEdit::multiline(&mut self.commit_message)
+                    .desired_width(ui.available_width())
+                    .ui(ui);
+                egui::containers::Sides::new().show(
+                    ui,
+                    |ui| {
+                        // todo
+                        ui.checkbox(&mut self.commit_ammend, "ammend");
+                    },
+                    |ui| {
+                        if ui.button("commit").clicked() {
+                            let mut index = self.repo.index().unwrap();
+                            let tree_oid = index.write_tree().unwrap();
+                            let tree = self.repo.find_tree(tree_oid).unwrap();
+
+                            let head = self.repo.head().unwrap();
+                            let parent_commit = head.peel_to_commit().unwrap();
+
+                            let commit_oid = self.repo.commit(
+                                Some("HEAD"),
+                                &self.repo.signature().unwrap(),
+                                &self.repo.signature().unwrap(),
+                                &self.commit_message,
+                                &tree,
+                                &[&parent_commit],
+                            ).unwrap();
+                            println!("Created commit: {:?}", commit_oid);
+                        }
+                    },
+                )
+            });
         });
     }
 }
